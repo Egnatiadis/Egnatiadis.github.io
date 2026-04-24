85aae944f2cff9cfe91bad20ac356c013aaa8410
Egnatiadis <146769667+Egnatiadis@users.noreply.github.com>
2026-04-24 15:25:27 +0200

    Update jms_console.c

--- jms_console.c
+++ jms_console.c
@@ -6,17 +6,47 @@
 
 int main(int argc, char *argv[]) {
     char *w, *r; int opt;
+
+    // Ανάγνωση παραμέτρων γραμμής εντολών για τα ονόματα των pipes [cite: 50]
     while ((opt = getopt(argc, argv, "w:r:")) != -1) {
-        if (opt == 'w') w = optarg; else if (opt == 'r') r = optarg;
+        // Το -w ορίζει το pipe γραφής προς τον coord [cite: 51]
+        if (opt == 'w') w = optarg; 
+        // Το -r ορίζει το pipe ανάγνωσης από τον coord [cite: 52]
+        else if (opt == 'r') r = optarg;
     }
-    int fdw = open(w, O_WRONLY), fdr = open(r, O_RDONLY);
+
+    // Άνοιγμα των named pipes jms_in και jms_out [cite: 19, 51, 52]
+    int fdw = open(w, O_WRONLY);
+    int fdr = open(r, O_RDONLY);
+
     char line[1024], res[4096];
+
+    // Διάβασμα εντολών από το standard input του χρήστη [cite: 55]
     while (fgets(line, sizeof(line), stdin)) {
-        line[strcspn(line, "\n")] = 0; if (strlen(line) == 0) continue;
+        
+        // Αφαίρεση του χαρακτήρα αλλαγής γραμμής
+        line[strcspn(line, "\n")] = 0; 
+        if (strlen(line) == 0) continue;
+
+        // Αποστολή της εντολής στον jms_coord μέσω του pipe [cite: 23, 73]
         write(fdw, line, strlen(line)+1);
+
+        // Τερματισμός αν ο χρήστης δώσει την εντολή εξόδου
         if (strcmp(line, "exit") == 0) break;
+
+        // Ανάγνωση της απάντησης από τον jms_coord [cite: 26, 73]
         int n = read(fdr, res, sizeof(res)-1);
-        if (n > 0) { res[n] = '\0'; printf("%s\n", res); }
+        
+        // Εκτύπωση του αποτελέσματος στην οθόνη του χρήστη [cite: 26]
+        if (n > 0) { 
+            res[n] = '\0'; 
+            printf("%s\n", res); 
+        }
     }
+
+    // Κλείσιμο των file descriptors πριν την έξοδο
+    close(fdw);
+    close(fdr);
+
     return 0;
 }
